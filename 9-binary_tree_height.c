394b60aab955c999e24aad86bb54aac9dcf8358b
Xdamix1991 <bourichalamine1@gmail.com>
2024-08-06 13:23:02 +0200

    fixed betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,6 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height;
 	size_t right_height;
+	
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
